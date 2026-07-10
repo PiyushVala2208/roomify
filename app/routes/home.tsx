8a127c0d9e7ebe89b9e62db2500bbe43b34bd967
PiyushVala2208 <piyushvala45@gmail.com>
2026-07-10 14:50:18 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../../components/Navbar";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Roomify" },
+    { name: "description", content: "Welcome to Roomify!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+    </div>
+  );
+}
