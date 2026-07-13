1403d5100c2a62d1f68a033a00fc6207db65f46a
PiyushVala2208 <piyushvala45@gmail.com>
2026-07-13 12:55:46 +0530

    implement puter workers, creation of 3D floor plan and creation of the project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,8 +4,8 @@ import type { Route } from "./+types/home";
 import Button from "../../components/ui/Button";
 import Upload from "../../components/Upload";
 import { useNavigate } from "react-router";
-import { useState } from "react";
-import { createProject } from "../../lib/puter.action";
+import { useEffect, useRef, useState } from "react";
+import { createProject, getProjects } from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -18,8 +18,13 @@ export default function Home() {
   const navigate = useNavigate();
   const [projects, setProjects] = useState<DesignItem[]>([]);
 
+  const isCreatingProjectRef = useRef(false);
+
   const handleUploadComplete = async (base64Image: string) => {
     try {
+      if (isCreatingProjectRef.current) return false;
+      isCreatingProjectRef.current = true;
+
       const newId = Date.now().toString();
       const name = `Residence ${newId}`;
 
@@ -54,8 +59,20 @@ export default function Home() {
       return true;
     } catch (e) {
       console.log(e);
+    } finally {
+      isCreatingProjectRef.current = false;
     }
   };
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const items = await getProjects();
+
+      setProjects(items);
+    };
+
+    fetchProjects();
+  }, []);
 
   return (
     <div className="home">
